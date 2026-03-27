c9c4a336582ead356d751379d2f87541fa73518e
yakumo16 <fei.su12@outlook.com>
2026-03-28 03:55:51 +0800

    “update”

---
+++ run_decode.sh
@@ -0,0 +1,8 @@
+python infer_avsr.py \
+  --gpu 0 \
+  --config path/to/config.yaml \
+  --data_type raw \
+  --test_audio_data path/to/audio_data.list \
+  --test_video_data path/to/video_data.list \
+  --checkpoint path/to/checkpoint \
+  --result_dir out_put
