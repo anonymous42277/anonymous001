b26bd0e2eded9084f5dd1c97964af4460ebaf187
anonymous42277 <anonymous42277@gmail.com>
2026-03-28 06:08:11 +0800

    update files

--- run_decode.sh
+++ run_decode.sh
@@ -6,3 +6,11 @@ python infer_avsr.py \
   --test_video_data path/to/video_data.list \
   --checkpoint path/to/checkpoint \
   --result_dir out_put
+
+
+python infer_llmavsr.py \
+  --gpu 0 \
+  --config path/to/config.yaml \
+  --test_unit_nbest_data path/to/units_nbest.list \
+  --checkpoint path/to/checkpoint.pt \
+  --result_dir output
